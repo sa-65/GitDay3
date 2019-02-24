d47d84303e7528a1a0c8061e45b4b375f6c9b1f8
sa-65 <46764145+sa-65@users.noreply.github.com>
2019-02-23 22:53:42 -0500

    Anything word added

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -33,7 +33,7 @@ public class LoginTest {
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
         String title=driver.getTitle();
-        Assert.assertEquals(title,"Web Orders");
+        Assert.assertEquals(title,"Web Orders","Anything");
 
 
 
@@ -46,7 +46,7 @@ public class LoginTest {
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
         driver.findElement(By.id("ctl00_logout")).click();
         String title=driver.getTitle();
-        Assert.assertEquals(title,"Web Orders Login");
+        Assert.assertEquals(title,"Web Orders Login","Anything");
 
 
 
